77f4c326446822de300351eaca0c3439e018b68e
paritoshrakshit583 <paritoshrakshit583@gmail.com>
2021-03-11 22:45:28 +0530

    For remote

---
+++ src/main/java/com/demo/microservices/springdatademoservice/SpringDataDemoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.microservices.springdatademoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataDemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataDemoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/microservices/springdatademoservice/SpringDataDemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.microservices.springdatademoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataDemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
